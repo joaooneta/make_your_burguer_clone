b3f867b4dc92e8b45d96a07d2324cafeb33ba5be
João Oneta <joaomatheusoneta01@gmail.com>
2022-04-18 18:14:09 -0300

    Implementação do Vue-router com criação das rotas / e /pedidos,juntamente com as views HomeView e PedidosView.

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<script>
+export default { name: "Footer" };
+</script>
+
+<template><h1>Eu sou um rodapé</h1></template>
+
+<style></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<script>
+export default { name: "Navbar" };
+</script>
+
+<template>
+  <nav id="nav">
+    <RouterLink to="/">
+      <img src="" alt="Logo Make Your Burguer" />
+    </RouterLink>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/pedidos">Pedidos</RouterLink>
+  </nav>
+</template>
+
+<style scoped></style>
